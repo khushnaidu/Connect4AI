fdcf595938f82e154e0a145674d7752dd27f0ecf
Tanmay Singal <tanmaysingal@icloud.com>
2023-12-09 22:08:25 +0000

    renamed variables

--- main.py
+++ main.py
@@ -22,125 +22,91 @@ def start_game():
             continue
 
 
-# Receiving matrix row and col and Creating empty board/matrix , also create columns indexes list and return them.
-def board_creating(matrix_rows: int, matrix_cols: int):
-    board_matrix = [[" " for _ in range(matrix_cols)] for _ in range(matrix_rows)]
-    columns_index_print = [
-        "-" + str(x) + "-" if x < 10 else str(x) for x in range(1, number_of_cols + 1)
+# Generate new game board and return index list of columns
+def generate_game_board(rows: int, cols: int):
+    game_board = [[" " for _ in range(cols)] for _ in range(rows)]
+    columns_list = [
+        " " + str(x) + " " if x < 10 else str(x) for x in range(1, cols + 1)
     ]
-    return board_matrix, columns_index_print
+    return game_board, columns_list
 
 
-def check_free_columns(matrix_board: list, matrix_cols: int):
-    free_cols = [
-        (index_col + 1)
-        for index_col in range(matrix_cols)
-        if matrix_board[0][index_col] == " "
+def check_free_columns(game_board: list, num_cols: int):
+    return [
+        (col + 1)
+        for col in range(num_cols)
+        if game_board[0][col] == " "
     ]
-    return free_cols
 
 
-# Take the user input and check it if is valid, return correct index for token place
-def player_token_placement(
-    p_symbol: str, player_name: str, free_columns_index: list, matrix
+# Ask user for col choice and place token on board
+def new_token_col(
+    player_name: str, free_columns_index: list, matrix
 ):
     if player_name == "AI":
         return monte_carlo_ai_placement(matrix)
 
     while True:
-        try:
-            column_index_place = int(
-                input(f"Choose a column to drop your token.\n => : ")
-            )
-
-            # If index is incorrect raise error
-            if column_index_place + 1 not in free_columns_index:
-                raise ValueError
-
-            # stop the loop and return correct index for placing the player symbol
-            return column_index_place - 1
-
-        # if error occurred print error message and show the free columns for placement
-        except ValueError:
+        col_choice = int(
+            input(f"Choose a column to drop your token.\n => : ")
+        )
+        if col_choice + 1 not in free_columns_index:
             print(
-                f"\nIncorrect input!"
-                f"\nChoose one of the free columns: {free_columns_index}"
-            )
+            f"\nIncorrect input!"
+            f"\nChoose one of the free columns: {free_columns_index}"
+            ) 
+        else:
+            return col_choice - 1
 
 
-# Place token, check for free spaces
-def place_token(board_matrix: list, matrix_rows: int, col_to_place: int, p_symbol: str):
-    # Starting form the bottom of the board/matrix
-    for current_row in range(matrix_rows - 1, -1, -1):
-        # There is no else case, there is at least one free space guaranteed from check_free_columns().
-        if board_matrix[current_row][col_to_place] == " ":
-            board_matrix[current_row][col_to_place] = p_symbol
-
-            # Creating variable for where was placed the last token.
-            last_row_col_placement = (current_row, col_to_place)
-
-            # Return updated matrix/board and the coordinates of the last token placed
-            return board_matrix, last_row_col_placement
+# Place a token on the board
+def place_token(game_board: list, num_rows: int, num_cols: int, player_symbol: str):
+    for current_row in range(num_rows - 1, -1, -1):
+        if game_board[current_row][num_cols] == " ":
+            game_board[current_row][num_cols] = player_symbol
+            last_row_col_placement = (current_row, num_cols)
+            return game_board, last_row_col_placement
 
 
 def winner_check(
-    matrix_board: list,
-    p_symbol: str,
-    last_r_c_token_placed: tuple,
-    matrix_rows: int,
-    matrix_cols: int,
+    game_board: list,
+    player_symbol: str,
+    last_token: tuple,
+    num_rows: int,
+    num_cols: int,
 ):
-    row, col = last_r_c_token_placed
+    row, col = last_token
 
-    # Win pattern, going in both direction in the matrix with positive or negative step( -1 , +1 ).
-    directions = (
-        # (R, C) # First direction | Second direction
-        (1, 0),  # Bottom - Top
-        (0, 1),  # Right - Left
-        (1, 1),  # Prime Diagonal
-        (1, -1),  # Secondary Diagonal
-    )
+    directions = ((1, 0),(0, 1),(1, 1),(1, -1),)
 
-    # Checking every possible direction for least 4 connected/same blocks
-    for dir_r, dir_c in directions:
-        # The last token itself is already 1 of 4 blocks , counter = 1
-        counter = 1
-
-        # Using positive or negative step to check in both direction of a row , column or diagonal
-        for direction_step_pos_or_neg in [1, -1]:
-            # Checking maximum tree blocks in direction is the limit for longest chain , or maximum 7 blocks connected.
-            for dir_step in range(1, 4):
-                #  Token index  row/col  + ( direction pattern * dir_step (1,2,3) * step (+1 , -1 )
-                new_r = row + dir_r * dir_step * direction_step_pos_or_neg
-                new_c = col + dir_c * dir_step * direction_step_pos_or_neg
-
-                # check if the new created indexes are in range of the matrix/board
-                if 0 <= new_r < matrix_rows and 0 <= new_c < matrix_cols:
-                    # if there is the same symbol there as the current player  counter add 1
-                    if matrix_board[new_r][new_c] == p_symbol:
-                        counter += 1
-                    # else stop checking in this direction
+    # Checking every possible direction for blocks
+    for x, y in directions:
+        count = 1
+        for dir in [1, -1]:
+            for step in range(1, 4):
+                x_new = row + x * step * dir
+                y_new = col + y * step * dir
+                if 0 <= x_new < num_rows and 0 <= y_new < num_cols:
+                    if game_board[x_new][y_new] == player_symbol:
+                        count += 1
                     else:
                         break
-
-        # if counter is at least 4 = four or more connected same elements , return True ( current player is Winner )
-        if counter >= 4:
+        if count >= 4:
             return True
-    # return False there is still no winner
     return False
 
 
-def another_game():
+def another_game_prompt():
     while True:
-        do_you_want_new_game = input("\nDo you want to play again [Y/N] ? \n => : ")
-        if do_you_want_new_game.lower() == "yes" or do_you_want_new_game.lower() == "y":
+        is_play_again = input("\nDo you want to play again [Y/N] ? \n => : ")
+        if is_play_again.lower() == "yes" or is_play_again.lower() == "y":
             return True
         elif (
-            do_you_want_new_game.lower() == "no" or do_you_want_new_game.lower() == "n"
+            is_play_again.lower() == "no" or is_play_again.lower() == "n"
         ):
             return False
         else:
-            print(f"\nIncorrect input!")
+            print(f"\Invalid input!")
 
 
 # Simple - MCTS - Model
@@ -155,7 +121,7 @@ def monte_carlo_ai_placement(matrix):
     for game_number in range(100):
         mc_matrix = copy.deepcopy(matrix)
         for counter in range(0, 100):
-            free_cols = check_free_columns(mc_matrix, number_of_cols)
+            free_cols = check_free_columns(mc_matrix, num_cols)
             random_place = random.choice(free_cols) - 1
 
             if counter == 0:
@@ -170,7 +136,7 @@ def monte_carlo_ai_placement(matrix):
 
             # AI Place random token
             mc_matrix, mc_r_c_last_token = place_token(
-                mc_matrix, number_of_rows, random_place, player_symbol
+                mc_matrix, num_rows, random_place, player_symbol
             )
 
             # Flags for Winner or Draw Game
@@ -178,12 +144,12 @@ def monte_carlo_ai_placement(matrix):
                 mc_matrix,
                 player_symbol,
                 mc_r_c_last_token,
-                number_of_rows,
-                number_of_cols,
+                num_rows,
+                num_cols,
             )
             mc_end_game_flag = (
                 False
-                if len(check_free_columns(mc_matrix, number_of_cols)) > 0
+                if len(check_free_columns(mc_matrix, num_cols)) > 0
                 else True
             )
 
@@ -207,99 +173,79 @@ def monte_carlo_ai_placement(matrix):
     return sorted_all_moves[0][0]
 
 
-def board_print(matrix_board: list, columns_print: list, matrix_cols: int):
+def print_game_board(game_board: list, cols_print: list, num_cols: int):
     print("\n\n\n\n\n\n\n")
 
-    # Columns with numbers
-    print("|" + "|".join(columns_print) + "|")
-
-    # Every mid row and matrix row.
+    print("|" + "|".join(cols_print) + "|")
     [
         print(
-            "|" + "---|" * (matrix_cols - 1) + "---|\n" + "|-" + "-|-".join(x),
+            "|" + "---|" * (num_cols - 1) + "---|\n" + "|-" + "-|-".join(x),
             end="-|\n",
         )
-        for x in matrix_board
+        for x in game_board
     ]
-
-    # Bottom frame
-    print("-" + "----" * (matrix_cols - 1) + "----\n")
-
-
-def winner_print(player_name: str):
-    print(f"{player_name} won!")
-
-
-def draw_print():
-    print("\n┌-------------DRAW-------------")
-
+    print("-" + "----" * (num_cols - 1) + "----\n")
 
 # Start game and initialize variables
 gameplay_mode = start_game()
-number_of_rows, number_of_cols = 6, 7
+num_rows, num_cols = 6, 7
 players_dict = {}
 players_dict["You"] = "x"  # Player
 players_dict["AI"] = "o"  # AI
-winner_flag = False  # Either player wins
-end_game_flag = False  # Draw
-new_game_flag = False  # New game
+is_winner = False  # Either player wins
+is_game_end = False  # Draw
+is_play_again = False  # New game
 
 # Create board
-board, columns_print_for_representation = board_creating(number_of_rows, number_of_cols)
+board, cols_print = generate_game_board(num_rows, num_cols)
 
 # While one of two condition is met [Winner] or [No more moves].
-while not winner_flag and not end_game_flag:
+while not is_winner and not is_game_end:
     # Using for-loop to rotate players turns.
     for player_name, players_symbol in players_dict.items():
-        board_print(board, columns_print_for_representation, number_of_cols)
+        print_game_board(board, cols_print, num_cols)
         free_columns = check_free_columns(
-            board, number_of_cols
-        )  # Check for free columns (cols w empty on top)
-        column_to_place = player_token_placement(
+            board, num_cols
+        )
+        column_to_place = new_token_col(
             players_symbol, player_name, free_columns, board
         )
 
-        # Placing the color token in the board
         board, r_c_last_token = place_token(
-            board, number_of_rows, column_to_place, players_symbol
+            board, num_rows, column_to_place, players_symbol
         )
 
         # Check if there is a winner ( based on last placed token )
-        winner_flag = winner_check(
-            board, players_symbol, r_c_last_token, number_of_rows, number_of_cols
+        is_winner = winner_check(
+            board, players_symbol, r_c_last_token, num_rows, num_cols
         )
 
         # Check if there is more empty spaces
-        end_game_flag = (
-            False if len(check_free_columns(board, number_of_cols)) > 0 else True
+        is_game_end = (
+            False if len(check_free_columns(board, num_cols)) > 0 else True
         )
 
-        # if any of the flag is raised stop the loop
-        if winner_flag or end_game_flag:
-            # Print the final state of the board
-            board_print(board, columns_print_for_representation, number_of_cols)
+        # exit on game end or start another
+        if is_winner or is_game_end:
+            print_game_board(board, cols_print, num_cols)
 
-            # Print Winner
-            if winner_flag:
-                winner_print(player_name)
+            if is_winner:
+                print(f"{player_name} won this round!")
+            elif is_game_end:
+                print("\n┌-------------DRAW-------------")
 
-            # Print Draw
-            elif end_game_flag:
-                draw_print()
-
-            # Option for new round available only in the end of the game
-            new_game_flag = another_game()
+            # Prompt user to play arouther round
+            is_play_again = another_game_prompt()
             break
 
-    # If new round is selected the game continue after the players name and color selection.
-    if new_game_flag:
-        # Reset all flags and create new empty board.
-        winner_flag = False
-        end_game_flag = False
-        new_game_flag = False
-        board, columns_print_for_representation = board_creating(
-            number_of_rows, number_of_cols
+    # Reset game for another round
+    if is_play_again:
+        is_winner = False
+        is_game_end = False
+        is_play_again = False
+        board, cols_print = generate_game_board(
+            num_rows, num_cols
         )
         continue
 
-print("\n\nThank you for playing Connect-4 made by me! Goodbye!")
+print("\n\nThank you for playing")
